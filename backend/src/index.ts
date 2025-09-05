66ac05c40ba0a6fddc590fd2239c1826cb462e92
Tugsibility <akimatsuchohideaki@gmail.com>
2025-09-05 19:01:32 +0800

    env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -54,7 +54,7 @@ app.use(
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: [process.env.FRONTEND_URL || "http://localhost:3000"],
     credentials: true,
   })
 );
